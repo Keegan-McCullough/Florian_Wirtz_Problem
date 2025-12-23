e441495c8a7e90489d81e63fc2715ef6ac44cf1e
Keegan-McCullough <keeganmcculloughfury@gmail.com>
2025-12-23 01:37:52 -0500

    Added some basic stats calculation

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
+def calculate_statistics(df):
+    goalsPerGame = (df['Goals'] / df['Appearances']).round(2)
+    assistsPerGame = (df['Assists'] / df['Appearances']).round(2)
+    df['GoalsXGame'] = goalsPerGame
+    df['AssistsXGame'] = assistsPerGame
+    return df
+
 if __name__ == "__main__":
     df = load_data("Stats.csv")
+    df = calculate_statistics(df)
     print(df.head())
